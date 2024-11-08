275126145b3226af93c8c4be4dc42f1a61e86767
SigurdFPS <rigginscody@gmail.com>
2024-11-08 00:11:50 -0600

    Update teamModel.js

--- models/teamModel.js
+++ models/teamModel.js
@@ -1,10 +1,14 @@
 // File: models/teamModel.js
-const mongoose = require('mongoose');
 
-const teamSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],  // Array of user IDs
-    maxSize: { type: Number, required: true, enum: [1, 2, 3] },  // 1 for solo, 2 for duo, 3 for trio
-});
+// In Supabase, use the SQL editor to create the following schema:
 
-module.exports = mongoose.model('Team', teamSchema);
+/*
+CREATE TABLE teams (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    maxSize INT CHECK (maxSize IN (1, 2, 3)),
+    members BIGINT[] -- Array of user IDs
+);
+*/
+
+// This file can be removed, as the database schema is now managed directly in Supabase.
