7c350d5d6df2d08d1a25ccb653be6a2eea8c2c86
SigurdFPS <rigginscody@gmail.com>
2024-11-08 00:12:39 -0600

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+// Supabase uses email for authentication, so ensure that email and password are handled
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 
